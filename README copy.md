6820a4155c57bc71e2dfebcab23d177da378f5c5
Daria Malina <dariamalinawork@gmail.com>
2025-03-17 12:56:38 +0300

    feat: add build app

--- README copy.md
+++
@@ -1,3 +0,0 @@
-# Flappy-Bird
-Remake of the Original FlappyBird Using **JS** &amp; **HTML Canvas** 
-# [Play it.](https://malinadaria.github.io/JS-Flappy-Bird/index.html)
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// webpack.config.js
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // Additional configuration goes here
+};
