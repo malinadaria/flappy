224964a17c7f0bf06c853dbab2a06b73bfb08709
Daria Malina <dariamalinawork@gmail.com>
2025-03-17 13:08:14 +0300

    feat: add build app

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 // webpack.config.js
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   target: 'node',
   entry: './index.js',
@@ -8,5 +8,28 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
+        use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Каталог для статики
+    },
+    open: true, // Автоматически открывать браузер
+  },
+
+  mode: 'development', // Режим сборки
   // Additional configuration goes here
 };
